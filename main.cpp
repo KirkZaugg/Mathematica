d350e0c29d3f94393b53c60f14ba82ea6c9d51f1
KirkZaugg <25kzaugg@go.dsdmail.net>
2024-09-03 14:15:57 +0000

    added operation stuff

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include<iostream>
 #include<math.h>
+#include<time.h>
+
+checkInput(double ans) {
+    std::cin >> double input;
+    return (input == ans);
+}
 
 int mainGame(int gamenum) {
     for(; gamenum > 0; gamenum--) {
         char operation = std::rand() % 5;        //0 = addition; 1 = subtraction; 2 = multiplication; 3 = division; 4 = modulus
-        
+        int a = std::rand() % 100;
+        int b = std::rand() % 100;
+        double c = 0;
+        switch operation {
+            case 0:
+                c = a + b;
+                operation = '+';
+                break;
+            case 1:
+                c = a - b;
+                operation = '-';
+                break;
+            case 2:
+                c = a * b;
+                operation = '*';
+                break;
+            case 3:
+                c = a / b;
+                operation = '/';
+                break;
+            case 4:
+                c = a % b;
+                operation = '%';
+                break;
+            default:
+                std::cout << "You done messed up.\n";
+        }
+        std::cout << "What is " << a << " " << operation << " " << b << "? ";
+        if(checkInput(c)) {
+            std::cout << "That is correct!\n";
+        } else {
+            std::cout << "That is incorrect!\n";
+        }
     }
 }
 
